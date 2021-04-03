a88a9f808e389fb5518d0bf2f80876665d1a262c
= <you@example.com>
2021-04-03 22:39:13 +0700

    Edit buku

--- lib/database/dbhelper.dart
+++ lib/database/dbhelper.dart
@@ -1,109 +1,132 @@
-import 'package:mobile_uts/pages/home.dart';
+import 'package:mobile_uts/pages/homeBuku.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:mobile_uts/model/item.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:mobile_uts/model/buku.dart';
+import 'package:mobile_uts/model/stok.dart';
 import 'dart:async';
 import 'dart:io';
 
 class DbHelper {
-
   static DbHelper _dbHelper;
   static Database _database;
   DbHelper._createObject();
-
   Future<Database> initDb() async {
-
-    //untuk menentukan nama database dan lokasi yg dibuat
     Directory directory = await getApplicationDocumentsDirectory();
-    String path = directory.path + 'stokatk.db';
+    String path = directory.path + 'tokobuku.db';
 
-    //create, read databases
-    var stokatDatabase = openDatabase(
-      path, version: 8, 
-      onCreate: _createDb,
-      onUpgrade: _onUpgrade
-    );
-
-    //mengembalikan nilai object sebagai hasil dari fungsinya
-    return stokatDatabase;
+    var tokobukuDatabase =
+        openDatabase(path, version: 10, onCreate: _createDb);
+    return tokobukuDatabase;
   }
 
-  void _onUpgrade(Database db, int oldVersion, int newVersion) {
+  void _onUpgrade(Database db, int oldVersion, int newVersion) async {
     _createDb(db, newVersion);
   }
 
-  //buat tabel baru dengan nama item
   void _createDb(Database db, int version) async {
-    await db.execute
-    ('''
-      CREATE TABLE buku 
-      (
-        idbuku INTEGER PRIMARY KEY AUTOINCREMENT,
-        judulbuku TEXT,
-        penerbit TEXT,
-        harga INTEGER,
-        stok INTEGER,
-        tahunterbit INTEGER,
-      )
-    ''');
+    var batch = db.batch();
+    batch.execute('DROP TABLE IF EXISTS buku');
+    batch.execute('DROP TABLE IF EXISTS stok');
+    batch.execute('''
+ CREATE TABLE buku (
+ idBuku INTEGER PRIMARY KEY AUTOINCREMENT,
+  kategoriBuku TEXT,
+  namaBuku TEXT,
+  penerbitBuku TEXT,
+  penulisBuku TEXT,
+  jumlahBuku INTEGER
+ )
+ ''');
+    batch.execute('''
+ CREATE TABLE anggota (
+ idAnggota INTEGER PRIMARY KEY AUTOINCREMENT,
+  namaAnggota TEXT,
+  jenisAnggota TEXT,
+  alamatAnggota TEXT,
+  nik INTEGER,
+  umur INTEGER
+ )
+ ''');
+    await batch.commit();
   }
 
-  //select databases
-  Future<List<Map<String, dynamic>>> select() async {
+  //fungsi untuk select database
+  Future<List<Map<String, dynamic>>> selectBook() async {
     Database db = await this.initDb();
-    var mapList = await db.query(
-      'barang', 
-      orderBy: 'kodeBrg'
-    );
+    var mapList = await db.query('book', orderBy: 'namaBuku');
     return mapList;
   }
 
-  //create databases
-  Future<int> insert(Buku object) async {
+  Future<List<Map<String, dynamic>>> selectAnggota() async {
     Database db = await this.initDb();
-    int count = await db.insert(
-      'barang', 
-      object.toMap()
-    );
+    var mapList = await db.query('anggota', orderBy: 'namaAnggota');
+    return mapList;
+  }
+
+  //fungsi untuk mengisi data pada tabel book
+  Future<int> insertBook(Book object) async {
+    Database db = await this.initDb();
+    int count = await db.insert('book', object.toMap());
     return count;
   }
 
-  //update databases
-  Future<int> update(Buku object) async {
+  //fungsi untuk mengisi data pada tabel anggota
+  Future<int> insertAnggota(Anggota object) async {
     Database db = await this.initDb();
-    int count = await db.update(
-      'barang', 
-      object.toMap(), 
-      where: 'id=?', 
-      whereArgs: [object.id]
-    );
+    int count = await db.insert('anggota', object.toMap());
     return count;
   }
 
-  //delete databases
-  Future<int> delete(Buku object) async {
+  //fungsi untuk update data tabel book
+  Future<int> updateBook(Book object) async {
     Database db = await this.initDb();
-    int count = await db.delete(
-      'barang', 
-      where: 'id=?', 
-      whereArgs: [object.id]
-    );
+    int count = await db.update('book', object.toMap(),
+        where: 'idBuku=?', whereArgs: [object.idBuku]);
     return count;
   }
 
-  Future<List<Buku>> getBarangList() async {
-    var bukuMapList = await select();
-    int count = bukuMapList.length;
-    // ignore: deprecated_member_use
-    List<Buku> itemList = List<Buku>();
+  //fungsi untuk update tabel anggota
+  Future<int> updateAnggota(Anggota object) async {
+    Database db = await this.initDb();
+    int count = await db.update('anggota', object.toMap(),
+        where: 'idAnggota=?', whereArgs: [object.idAnggota]);
+    return count;
+  }
+  //fungsi untuk menghapus data tabel book
+
+  Future<int> deleteBook(int idBuku) async {
+    Database db = await this.initDb();
+    int count = await db.delete('book', where: 'idBuku=?', whereArgs: [idBuku]);
+    return count;
+  }
+  //fungsi untuk menghapus data tabel book
+
+  Future<int> deleteAnggota(int idAnggota) async {
+    Database db = await this.initDb();
+    int count = await db
+        .delete('anggota', where: 'idAnggota=?', whereArgs: [idAnggota]);
+    return count;
+  }
+
+  //fungsi untuk mengembalikan nilai data data yang baru dimasukkan
+  Future<List<Book>> getBookList() async {
+    var bookMapList = await selectBook();
+    int count = bookMapList.length;
+    List<Book> bookList = List<Book>();
     for (int i = 0; i < count; i++) {
-      itemList.add(
-        Buku.fromMap(
-          bukuMapList[i]
-        )
-      );
+      bookList.add(Book.fromMap(bookMapList[i]));
     }
-    return itemList;
+    return bookList;
+  }
+
+  Future<List<Anggota>> getAnggotaList() async {
+    var anggotaMapList = await selectAnggota();
+    int count = anggotaMapList.length;
+    List<Anggota> anggotaList = List<Anggota>();
+    for (int i = 0; i < count; i++) {
+      anggotaList.add(Anggota.fromMap(anggotaMapList[i]));
+    }
+    return anggotaList;
   }
 
   factory DbHelper() {
@@ -112,7 +135,6 @@ class DbHelper {
     }
     return _dbHelper;
   }
-
   Future<Database> get database async {
     if (_database == null) {
       _database = await initDb();
--- lib/model/buku.dart
+++ lib/model/buku.dart
@@ -1,36 +1,36 @@
 class Buku {
   //pembuatan atribut pada table book
   int _idBuku;
-  String _kategoriBuku;
   String _namaBuku;
+  String _kategoriBuku;
   String _penerbitBuku;
-  int _tahun;
+  int _tahunBuku;
   int _jumlahBuku;
 
-  Buku(this._kategoriBuku, this._namaBuku, this._penerbitBuku, this._tahun, this._jumlahBuku);
+  Buku(this._kategoriBuku, this._namaBuku, this._penerbitBuku, this._tahunBuku, this._jumlahBuku);
   // konstruktor konversi dari Map ke Item
   Buku.fromMap(Map<String, dynamic> map) {
     this._idBuku = map['idBuku'];
-    this._kategoriBuku = map['kategoriBuku'];
     this._namaBuku = map['namaBuku'];
+    this._kategoriBuku = map['kategoriBuku'];
     this._penerbitBuku = map['penerbitBuku'];
-    this._tahun = map['tahun'];
+    this._tahunBuku = map['tahunBuku'];
     this._jumlahBuku = map['jumlahBuku'];
   }
   // pembuatan setter getter
   int get idBuku => _idBuku;
 
-  String get kategoriBuku => this._kategoriBuku;
-  set kategoriBuku(String value) => this._kategoriBuku = value;
-
   String get namaBuku => this._namaBuku;
   set namaBuku(String value) => this._namaBuku = value;
 
+  String get kategoriBuku => this._kategoriBuku;
+  set kategoriBuku(String value) => this._kategoriBuku = value;
+  
   String get penerbitBuku => this._penerbitBuku;
   set penerbitBuku(String value) => this._penerbitBuku = value;
 
-  get tahun => this._tahun;
-  set tahun(value) => this._tahun = value;
+  get tahunBuku => this._tahunBuku;
+  set tahunBuku(value) => this._tahunBuku = value;
 
   get jumlahBuku => this._jumlahBuku;
   set jumlahBuku(value) => this._jumlahBuku = value;
@@ -38,10 +38,10 @@ class Buku {
   Map<String, dynamic> toMap() {
     Map<String, dynamic> map = Map<String, dynamic>();
     map['idBuku'] = this._idBuku;
-    map['kategoriBuku'] = kategoriBuku;
     map['namaBuku'] = namaBuku;
+    map['kategoriBuku'] = kategoriBuku;
     map['penerbitBuku'] = penerbitBuku;
-    map['tahun'] = tahun;
+    map['tahunBuku'] = tahunBuku;
     map['jumlahBuku'] = jumlahBuku;
     return map;
   }
