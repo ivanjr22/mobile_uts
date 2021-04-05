84aec8f6be5d7b0d3c59fdfbac7e889d28bbb37a
= <you@example.com>
2021-04-05 12:17:04 +0700

    Update Screenshot

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Tambah',
+      title: 'Toko Buku Sarjana',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
